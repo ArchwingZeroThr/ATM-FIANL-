c604e8d16d31c9ae656143cb5b75916837de29eb
ArchwingZeroThr <165612830+ArchwingZeroThr@users.noreply.github.com>
2024-04-25 16:49:54 +0800

    Update card_olg.sql

--- card_olg.sql
+++ card_olg.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `olg`
 --
-
+use card;
 DROP TABLE IF EXISTS `olg`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
